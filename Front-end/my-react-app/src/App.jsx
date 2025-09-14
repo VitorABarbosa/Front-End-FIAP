282306c961a8b86e4571e7189bfe5de2df67696e
Vitor <vitor.barbosa232006@gmail.com>
2025-09-14 13:42:06 -0300

    Criações de componentes com atualizações baseadas no estado anterior ( uma forma segura ), além de testar efeitos, eventos, Condicionais e listas.

--- Front-end/my-react-app/src/App.jsx
+++ Front-end/my-react-app/src/App.jsx
@@ -1,12 +1,8 @@
-import UsuarioInfo from "./components/UsuarioInfo";
-import ListaNumeros from "./components/ListaNumeros";
+import ListaComIds from "./components/ListaComIds";
 export default function App() {
   return (
     <main style={{ fontFamily: "sans-serif", padding: 16 }}>
-      <h1>Estado com Objetos e Arrays</h1>
-      <UsuarioInfo />
-      <hr />
-      <ListaNumeros />
+      <ListaComIds />
     </main>
   );
 }
